249b8bc89b3cf93cb3807d4c1486aaff0c0a86f8
abhaya264 <abhya264@gmail.com>
2023-01-04 19:21:36 +0530

    Inserted 'New', 'Open','Save' and 'Save as'

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,25 +21,52 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionNew_triggered()
 {
-
+    file_path="";
+    ui->textEdit->setText("");
+    flag=0;
 }
 
 
 void MainWindow::on_actionOpen_triggered()
 {
-
+    QString file_name= QFileDialog::getOpenFileName(this,"Open file");
+    QFile file(file_name);
+    if(!file.open(QFile::ReadOnly|QFile::Text)){
+        return;
+    }
+    file_path=file_name;
+    QTextStream in(&file);
+    QString text= in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
 }
 
 
 void MainWindow::on_actionSave_triggered()
 {
-
+    if(!flag) {on_actionSave_as_triggered();flag=1;return;}
+    QFile file(file_path);
+    QTextStream out(&file);
+    QString text= ui->textEdit->toPlainText();
+    out<<text;
+    file.flush();
+    file.close();
 }
 
 
 void MainWindow::on_actionSave_as_triggered()
 {
-
+    QString file_name= QFileDialog::getSaveFileName(this,"Save file");
+    QFile file(file_name);
+    if(!file.open(QFile::WriteOnly|QFile::Text)){
+        return;
+    }
+    file_path=file_name;
+    QTextStream out(&file);
+    QString text= ui->textEdit->toPlainText();
+    out<<text;
+    file.flush();
+    file.close();
 }
 
 
@@ -59,7 +90,11 @@ void MainWindow::on_actionPaste_triggered()
 
 void MainWindow::on_actionAbout_Notepad_triggered()
 {
-
+    QString abt;
+    abt="Author : Shyam \n";
+    abt+="A simple notepad app to get familiar with qt environment \n";
+    abt+= "@Copyright 2022-23";
+    QMessageBox::about(this,"About Notepad",abt);
 }
 
 
